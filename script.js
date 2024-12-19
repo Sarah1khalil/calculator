1035672c0340d84066e87d916fce12679576af8a
Sarah1khalil <codebraver62@codebrave.org>
2024-12-19 18:26:20 +0200

    my files

---
+++ script.js
@@ -0,0 +1,10 @@
+const output = document.querySelector("output");
+const result = document.querySelector("result");
+const keys = document.querySelectorAll("button");
+
+keys.forEach(element => {
+keys.addEventlistner('click',calculator)    
+})
+function calculator(){
+
+}
